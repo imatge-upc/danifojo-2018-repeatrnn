218047aa2ead73855a5b1433b8210e8beadddaa6
DaniFojo <dani.fojo@gmail.com>
2017-11-02 13:37:30 +0100

    Better test

--- test.py
+++ test.py
@@ -15,12 +15,22 @@ input_size = 3
 hidden_size = 5
 output_size = 2
 learning_rate = 0.01
-steps = 100
+steps = 1000
 lamda = 1e-3
 
-x = Variable(torch.randn(batch_size, sequence_length, input_size))
-y = Variable(x.data[:, :, :output_size]*3)
-h = Variable(torch.randn(1, batch_size, hidden_size))
+
+def generate():
+    x = Variable(torch.randn(batch_size, sequence_length, input_size))
+    y = Variable(torch.zeros(batch_size, sequence_length, output_size), requires_grad=False)
+    A = torch.Tensor([[1, 2, 3], [4, 5, 6]])
+    for i in range(batch_size):
+        for j in range(sequence_length):
+            y.data[i, j, :] = torch.mm(A, x.data[i, j, :].view(input_size, 1))
+    return x, y
+
+
+x, y = generate()
+h = Variable(torch.zeros(1, batch_size, hidden_size))
 arnn = ARNN(input_size, hidden_size, output_size, batch_first=True)
 o, p = arnn(x, h)
 print(o)
@@ -32,11 +42,16 @@ losses = np.zeros(steps)
 
 for i in range(steps):
     arnn.zero_grad()
+    x, y = generate()
     o, p = arnn(x, h)
     loss = criterion(o, y) + lamda*torch.sum(p)
     losses[i] = loss.data[0]
     loss.backward()
     optimizer.step()
 
+x, y = generate()
+o, p = arnn(x, h)
+print(o)
+print(y)
 plt.plot(losses)
 plt.show()
